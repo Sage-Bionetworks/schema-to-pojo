d1380510d44f577fde2d5d88abb4ab5dd033e6ac
john-hill <john.hill@sagebase.org>
2011-09-16 21:44:21 +0000

    Trying to fix unmappable character for encoding ASCII

--- schema-to-pojo-lib/src/main/java/org/sagebionetworks/schema/util/CompareUtils.java
+++ schema-to-pojo-lib/src/main/java/org/sagebionetworks/schema/util/CompareUtils.java
@@ -6,13 +6,13 @@ package org.sagebionetworks.schema.util;
 public class CompareUtils {
 	
 	/**
-	 * The absolute error used for comparing two doubles (epsilon – absolute error).
+	 * The absolute error used for comparing two doubles (epsilon - absolute error).
 	 */
 	public static final double ABSOLUTE_ERROR = 0.000000000000001d;
 	
 	/**
 	 * Since GWT does not support Double.doubleToLongBits() for comparing doubles, we needed an alternative.
-	 * This method simply compares two doubles with epsilon – absolute error.
+	 * This method simply compares two doubles with epsilon - absolute error
 	 * 
 	 *  @see <a href="Comparing floating point numbers">http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm</a>
 	 * @param a
@@ -20,7 +20,7 @@ public class CompareUtils {
 	 * @return
 	 */
 	public static boolean doubleEquals(double a, double b){
-		// epsilon – absolute error.
+		// epsilon - absolute error
 		return Math.abs(a-b) < ABSOLUTE_ERROR;
 	}
 }
