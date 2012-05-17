46aa894fd1694153ae86062bb716d248e893114e
john-hill <john.hill@sagebase.org>
2012-05-17 15:36:40 -0700

    schema-to-pojo-maven-plugin no longer has the root pom as its 'parent' This root pom.xml was not getting pushed to Artifactory (as it is not a plugin) causing clients problems with depending on the plugin.
    Also, created the 0.1.3-SNAPSHOT.
    
    Also, cleaned up the pomVersioner.py to be more generic.

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,27 +4,21 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.1.20'
-newVersion = '0.1.21'
+oldVersion = '0.1.21'
+newVersion = '0.1.3-SNAPSHOT'
 #Path to PLFM on your system
-startPath = 'C:\\sage\\workspace\\\jstp'
+startPath = os.getcwd()
+count = 0;
 for root, subFolders, files in os.walk(startPath):    
     for file in files:
         if file == 'pom.xml': 
             f = os.path.join(root, file) 
-            print 'updating '+f
+            print 'checking '+f
             for line in fileinput.FileInput(f, inplace=1):
                 found = False
                 if line.find('<version>'+oldVersion+'</version>') >= 0 and not found:
                     line = line.replace(oldVersion,newVersion)
                     found = True
+                    count += 1
                 print line.rstrip()
-        if file == 'DESCRIPTION' and root.find('rSynapseClient') >= 0:
-            f = os.path.join(root, file)
-            print 'updating '+f
-            for line in fileinput.FileInput(f, inplace=1):
-                found = False
-                if not found and line.find('Version') >= 0:
-                    line = 'Version: '+newVersion+'-0'
-                    found = True
-                print line.rstrip()
+print 'updated: '+str(count)+' pom.xml files'
