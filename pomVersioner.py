454afe842690eac885ea255794dcd8887ac8750c
John Hill <john.hill@sagebase.org>
2012-12-05 16:40:44 -0800

    Corrected the version number to 0.2.0

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,8 +4,8 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.1.4.1'
-newVersion = '0.1.4.2'
+oldVersion = '0.1.4.2'
+newVersion = '0.2.0'
 #Path to PLFM on your system
 startPath = os.getcwd()
 count = 0;
