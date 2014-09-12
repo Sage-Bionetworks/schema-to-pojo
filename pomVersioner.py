1599f656787ef86313dc8574a4a6240c57f36532
Jay Hodgson <jay.hodgson@sagebase.org>
2014-09-12 12:28:50 -0700

    bump version (really this time) to 0.3.7

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,8 +4,8 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.3.4'
-newVersion = '0.3.5'
+oldVersion = '0.3.6'
+newVersion = '0.3.7'
 #Path to PLFM on your system
 startPath = os.getcwd()
 count = 0;
